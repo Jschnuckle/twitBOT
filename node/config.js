7165c5c869916286d3b5a5991ae0bbd3e3ff3c31
Jschnuckle <33470769+Jschnuckle@users.noreply.github.com>
2019-05-20 11:37:39 -0500

    Create config.js

---
+++ node/config.js
@@ -0,0 +1,9 @@
+//a mini package to store the API keys
+module.exports = {
+
+    consumer_key:         'imrGrJ3L2KBKV89MC8HoJORSR',
+    consumer_secret:      'lYXQT1zRpdXvQEXhfTXJhwLermr6b3reBPpOOHdO9hAIHLg8Fv',
+    access_token:         '419110709-PwoXg6vuVORCmemw51Ysg0fagPGXlSgoqHUXEvAw',
+    access_token_secret:  'dS19qlQgw8gcvBN8Xo8pdExoJvOYnCRQcJCp3YBaGkYyx',
+
+}
